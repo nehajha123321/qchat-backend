8c497ef35962dee49e4d6459f7b7ae80cf6d0592
nehajha123321 <nehanejh@amazon.com>
2024-08-29 14:20:57 +0530

    change in configuration git url

--- amplify/custom/qchat-api/config/configuration.ts
+++ amplify/custom/qchat-api/config/configuration.ts
@@ -16,10 +16,12 @@ const _config: AppConfig = {
     APPRUNNER_GITHUB_URL: 'https://github.com/nehajha123321/qchat-backend',
     APPRUNNER_GITHUB_BRANCH_MAIN: 'main',
     APPRUNNER_GITHUB_BRANCH_SANDBOX: 'polly-integration',
-    APPRUNNER_GITHUB_CONNECTION_ARN: 'arn:aws:apprunner:ap-south-1:717937492408:connection/AppRunner-Github/a0c93386c8d549d3a906990c118db658',
+    APPRUNNER_GITHUB_CONNECTION_ARN: 'arn:aws:apprunner:ap-south-1:339046971378:connection/qchat-backend-connection/f610a6a7f0d5402f9336ec3d871bb369',
     KENDRA_INDEXID: "a91c1b65-2547-47cc-b9c0-ab0dad8a58b0",
-    CHAT_PROD_API: "https://chat.cloudevangelist.in?bedrockEngine=true&token=",
+    CHAT_PROD_API: "https://chat.cloudevangelistnew.in?bedrockEngine=true&token=",
 };
+
+
 
 // Export the configuration
 export default _config;
